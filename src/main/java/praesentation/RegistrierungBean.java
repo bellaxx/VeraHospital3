5cfcb88930d3f19855a16de5e8b04887ed2aa3e0
bellaxx <arabella.jackszis@study.hs-duesseldorf.de>
2017-06-22 22:56:31 +0200

    Registrierung implementiert

---
+++ src/main/java/praesentation/RegistrierungBean.java
@@ -0,0 +1,62 @@
+package praesentation;
+
+import javax.faces.bean.ManagedBean;
+import businesslogic.UserManager;
+
+@ManagedBean
+public class RegistrierungBean {
+	String name;
+	String fachgebiet;
+	String buero;
+	String email;
+	String passwort;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setFachgebiet(String fachgebiet){
+		this.fachgebiet = fachgebiet;
+	}
+	
+	public String getFachgebiet(){
+		return this.fachgebiet;
+	}
+	
+	public void setBuero(String buero){
+		this.buero = buero;
+	}
+	
+	public String getBuero(){
+		return buero;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getEmail(){
+		return this.email;
+	}
+	
+	public void setPasswort(String passwort){
+		this.passwort= passwort;
+	}
+	
+	public String getPasswort(){
+		return this.passwort;
+	}
+	
+	public String registrieren(){
+		UserManager.addArzt(this.name, this.fachgebiet, this.buero, this.email, this.passwort);
+		return "meinePatienten.xhtml";
+	}
+	
+	public String login(){
+		return "login.xhtml";
+	}
+}
